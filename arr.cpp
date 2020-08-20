c3f86d9db74d4af35a5b6cde3d75ea659e45736f
Charan <45995406+Charanvk@users.noreply.github.com>
2020-08-20 14:34:50 +0530

    Add files via upload

---
+++ arr.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int arr[6]={17,18,5,4,6,1};
+	int j,n=6;
+	int place,count=0;
+	for(int i=0;i<n;i++)
+	{
+		 j=0;
+		if(arr[j]>arr[i+1])
+		arr[j]==arr[i+1];
+		
+	}
+	cout<<arr[j];
+}
